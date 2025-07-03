c52adc54c8510f6ccb01d65c1fb487730f1ac000
sritha2223 <ragasritha71@gmail.com>
2025-07-03 10:10:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import FeedbackForm from "./components/FeedbackForm";
+import ReviewList from "./components/ReviewList";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <h1>🎬 Movie Feedback App</h1>
+        <nav className="nav">
+          <Link to="/feedback">Give Feedback</Link>
+          <Link to="/reviews">View Reviews</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Navigate to="/feedback" />} />
+          <Route path="/feedback" element={<FeedbackForm />} />
+          <Route path="/reviews" element={<ReviewList />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
